97d0cf919e6f7bf8bb0181f41c5e3b55f71171f5
Shivam Bhardwaj <shivambhardwaj13579@gmail.com>
2024-04-14 21:27:45 +0530

    adding selection table

---
+++ CC_Selection.sql
@@ -0,0 +1,25 @@
+select * from creditcard.customer;
+select * from creditcard.credit_card_type;
+select * from creditcard.offers;
+select * from creditcard.cust_card;
+select * from creditcard.transaction;
+select * from creditcard.offers_used;
+select * from creditcard.billing;
+
+
+
+delete from creditcard.customer;
+delete from creditcard.credit_card_type;
+delete from creditcard.offers;
+delete from creditcard.cust_card;
+
+
+select * from creditcard.customer_audit;
+select * from creditcard.credit_card_type_audit;
+select * from creditcard.offers_audit;
+select * from creditcard.cust_card_audit;
+
+delete from creditcard.customer_audit;
+delete from creditcard.credit_card_type_audit;
+delete from creditcard.offers_audit;
+delete from creditcard.cust_card_audit;
